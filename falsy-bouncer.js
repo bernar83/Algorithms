b87edd59e169f3cb10136d482413a1a3d85f7a47
Adrian Bernardo <nabernardo93@gmail.com>
2017-08-04 18:44:47 -0700

    Update falsy-bouncer.js

--- falsy-bouncer.js
+++ falsy-bouncer.js
@@ -1,9 +1,10 @@
+// return truthy values in an array
 function bouncer(arr) {
+  // filter through each element in given array
   var trueArr = arr.filter(function(val) {
-    if (arr[i] == true) {
-      return arr[i];
-    }
+	return val;
   });
+  return trueArr;
 }
 
 bouncer([7, "ate", "", false, 9]);
