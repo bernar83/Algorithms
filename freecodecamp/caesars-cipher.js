d85a41b40714ea561ff7536d867f9b61c0d62f40
Adrian Bernardo <nabernardo93@gmail.com>
2017-08-16 17:06:49 -0700

    Update caesars-cipher.js

--- freecodecamp/caesars-cipher.js
+++ freecodecamp/caesars-cipher.js
@@ -1,19 +1,29 @@
+// return decryption
 function rot13(str) { // LBH QVQ VG!
   var string = str;
   var _string = [];
   
   for (var i = 0; i < string.length; i++) {
-    if (string[i] == "!" || string[i] == "?" || string[i] == ".") {
-      var keep = string[i];
-      _string[i] = keep;
+    // if character is non-alphabetic then keep same character's unicode
+    if (string[i] == "!" || string[i] == "?" || string[i] == "." || string[i] == " ") {
+      _string[i] = string.charCodeAt(i);
     }
+    // change letter to decrypted letter's unicode
     else {
-      _string[i] = string.charCodeAt(i) + 13;
+      _string[i] = ((string.charCodeAt(i) - 65 + 13) % 26) + 65;
     }
   }
   
-  return _string;
+  var cipher = "";
+  
+  // change unicode back to letter
+  for (var j = 0; j < _string.length; j++) {
+    cipher += String.fromCharCode(_string[j]);
+  }
+  
+  return cipher;
 }
 
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!")
