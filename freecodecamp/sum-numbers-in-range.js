91d167739bf7b143cd1f76edadd2091620174ffc
Adrian Bernardo <nabernardo93@gmail.com>
2017-08-22 13:03:02 -0700

    Create sum-numbers-in-range.js

---
+++ freecodecamp/sum-numbers-in-range.js
@@ -0,0 +1,10 @@
+function sumAll(arr) {
+  var low = Math.min(arr[0], arr[1]);
+  var high = Math.max(arr[0], arr[1]);
+  var list = [low, high];
+  list.reduce(function(sum, value) {
+    
+  });
+}
+
+sumAll([1, 4]);
