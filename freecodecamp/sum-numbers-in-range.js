0a1cb4734a721a43f13fffc356f9e82eff8c7bd6
Adrian Bernardo <nabernardo93@gmail.com>
2017-08-23 17:11:39 -0700

    Update sum-numbers-in-range.js

--- freecodecamp/sum-numbers-in-range.js
+++ freecodecamp/sum-numbers-in-range.js
@@ -1,10 +1,26 @@
+// return sum of the numbers in array and in between
 function sumAll(arr) {
+  // find lowest number in array
   var low = Math.min(arr[0], arr[1]);
+  
+  // find highest number in array
   var high = Math.max(arr[0], arr[1]);
-  var list = [low, high];
-  list.reduce(function(sum, value) {
-    
+  
+  var list = [];
+  var count = 0;
+  
+  // sort array and include numbers in between
+  for (var i = low; i <= high; i++) {
+    list[count++] = i;
+  }
+  
+  // find the sum
+  var sum = list.reduce(function(sum, value) {
+    return sum + value;
   });
+  
+  return sum;
 }
 
 sumAll([1, 4]);
+sumAll([10, 5])
