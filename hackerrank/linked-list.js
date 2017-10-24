91b559da4f7b93928fc8e3557ad834587ceb6a5c
Adrian Bernardo <nabernardo93@gmail.com>
2017-10-24 08:49:32 -0700

    Update linked-list.js

--- hackerrank/linked-list.js
+++ hackerrank/linked-list.js
@@ -1,9 +1,19 @@
 function print(head) {
+    // if input head is not null
     if (head != null) {
+        
+        // initialize cursor to head of list
         var cursor = head;
+        
+        // show head's data
+        console.log(cursor.data);
+        
+        // point cursor to next node as long as it's not null
         while (cursor.next != null) {
-            console.log(cursor.data);
             cursor = cursor.next;
+            
+            // show the data where cursor is at
+            console.log(cursor.data);
         }
     }
 }
