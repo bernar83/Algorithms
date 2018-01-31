c1a17e16ca9a0f80b494362d97d56974e01a9e25
Adrian Bernardo <nabernardo93@gmail.com>
2018-01-31 10:59:59 -0800

    Create qheap1.js

---
+++ hackerrank/qheap1.js
@@ -0,0 +1,16 @@
+// Number -> String
+
+function processData(input) {
+    console.log(input.split('\n'));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
