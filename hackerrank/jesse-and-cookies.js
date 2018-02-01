b7687f6f054509732db735ea8b6810b80c51ba73
Adrian Bernardo <nabernardo93@gmail.com>
2018-02-01 11:23:42 -0800

    Create jesse-and-cookies.js

---
+++ hackerrank/jesse-and-cookies.js
@@ -0,0 +1,17 @@
+function processData(input) {
+    // Natural -> Natural
+    // produce the number of operations it takes to get all cookies to be >= to K
+    
+    console.log(0);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
