fc3ec31c129a3798555c2101267aeb76ccdf33d6
Adrian Bernardo <nabernardo93@gmail.com>
2018-06-15 12:06:05 -0700

    Update super-reduced-string.js

--- hackerrank/super-reduced-string.js
+++ hackerrank/super-reduced-string.js
@@ -1,18 +1,11 @@
 function super_reduced_string(s) {
-    var same = true;
-    const lettersArr = s.sort().reduce((acc, curr, currIndex) => {
-        if (currIndex < s.length - 1 && curr != s[currIndex + 1]) {
-            acc.concat(cur);
-            same = false;
-        }
-        else {
-            same = true;
-        }
-        return acc;
-    }, "");    
-
-    function getString(lettersArr.toString()) {
-        return (s === "") ? "Empty String" : s;
-    }
-    console.log(getString(s));
+   const result = s.sort().reduce((acc, curr) => {
+       const length = acc.length;
+       var regex = new RegExp(`{curr}`, 'g');
+       var found = s.match(regex);
+       
+       if (found.length % 2 === 1 || length === 0 || accumulator[length - 1] !== current) {
+           acc.concat(curr);
+       }
+   }, []);
 }
