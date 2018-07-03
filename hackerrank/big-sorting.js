8b854c26a3898bd1a963c79f48d667c89e833f62
Adrian Bernardo <nabernardo93@gmail.com>
2018-07-03 10:45:53 -0700

    Update big-sorting.js

--- hackerrank/big-sorting.js
+++ hackerrank/big-sorting.js
@@ -1,4 +1,13 @@
 function bigSorting(unsorted) {
+    var strGreaterThan16 = unsorted.filter(function(num, index) {
+        var numAndIndex = [];
+        if (num.length > 16) {
+            
+        }
+    });
+    if (strGreaterThan16.length > 0) {
+        
+    }
     var arrOfNums = unsorted.map(x => parseInt(x));
     var arrOfSortedNums = arrOfNums.sort((a, b) => a - b);
     return arrOfSortedNums;
